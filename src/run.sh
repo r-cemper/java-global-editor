e7317e76531eee300a21ed240aad769382f1ff72
r-cemper <146277387+r-cemper@users.noreply.github.com>
2023-11-17 22:03:03 +0100

    JDBC versions change to fast

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,6 @@
 
 echo "Building..."
 cd /opt/irisapp
-javac -classpath /usr/irissys/dev/java/lib/1.8/intersystems-jdbc-3.7.1.jar -d . /rcc/src/gedi.java
+javac -classpath /usr/irissys/dev/java/lib/1.8/intersystems-jdbc-3.*.jar -d . /rcc/src/gedi.java
 echo "Executing..."
 java gedi
